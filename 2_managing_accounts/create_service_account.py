c24a135daa7d3a993fc0378902c3305108c7400f
Craig Jones <craig@strongdm.com>
2020-07-16 18:21:40 -0500

    2_managing_accounts: added managing accounts example

---
+++ 2_managing_accounts/create_service_account.py
@@ -0,0 +1,34 @@
+# Copyright 2020 StrongDM Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import os
+import strongdm
+
+#	Load the SDM API keys from the environment.
+#	If these values are not set in your environment,
+#	please follow the documentation here:
+#	https://www.strongdm.com/docs/admin-guide/api-credentials/
+api_access_key = os.getenv("SDM_API_ACCESS_KEY")
+api_secret_key = os.getenv("SDM_API_SECRET_KEY")
+client = strongdm.Client(api_access_key, api_secret_key, host="api.strongdmdev.com:443")
+
+service = strongdm.Service(
+    name="example service account"
+)
+
+response = client.accounts.create(service, timeout=30)
+
+print("Successfully created service.")
+print("\tName: ", response.account.name)
+print("\tID: ", response.account.id)
---
+++ 2_managing_accounts/create_user.py
@@ -0,0 +1,36 @@
+# Copyright 2020 StrongDM Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import os
+import strongdm
+
+#	Load the SDM API keys from the environment.
+#	If these values are not set in your environment,
+#	please follow the documentation here:
+#	https://www.strongdm.com/docs/admin-guide/api-credentials/
+api_access_key = os.getenv("SDM_API_ACCESS_KEY")
+api_secret_key = os.getenv("SDM_API_SECRET_KEY")
+client = strongdm.Client(api_access_key, api_secret_key, host="api.strongdmdev.com:443")
+
+user = strongdm.User(
+    email="example@strongdm.com",
+    first_name="example",
+    last_name="example",
+)
+
+response = client.accounts.create(user, timeout=30)
+
+print("Successfully created user.")
+print("\tEmail: ", response.account.email)
+print("\tID: ", response.account.id)
