84e48b96f3946e2328872b17d3261a46fc49c611
Phil Hassey <phil@galcon.com>
2020-07-29 10:58:35 -0600

    3_managing_roles: add update role example

---
+++ 3_managing_roles/update_role.py
@@ -0,0 +1,46 @@
+# Copyright 2020 StrongDM Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import os
+import strongdm
+
+# Load the SDM API keys from the environment.
+# If these values are not set in your environment,
+# please follow the documentation here:
+# https://www.strongdm.com/docs/admin-guide/api-credentials/
+api_access_key = os.getenv("SDM_API_ACCESS_KEY")
+api_secret_key = os.getenv("SDM_API_SECRET_KEY")
+client = strongdm.Client(api_access_key, api_secret_key)
+
+# Create a role
+role = strongdm.Role(
+    name="example role",
+)
+create_response = client.roles.create(role, timeout=30)
+print("Successfully created role.")
+print("\tID: ", create_response.role.id)
+print("\tName: ", create_response.role.name)
+
+# Get the role
+get_response = client.roles.get(create_response.role.id, timeout=30)
+role = get_response.role
+
+# Set fields
+role.name = "example role updated"
+
+# Update the role
+update_response = client.roles.update(role, timeout=30)
+print("Successfully updated role.")
+print("\tID: ", update_response.role.id)
+print("\tName: ", update_response.role.name)
